4e7cfcc0e3d3691912a224c76792906d84dc3e2e
jiangtao <jiangtao@jiangtaodeMacBook-Pro.local>
2018-07-17 10:28:36 +0800

    master

---
+++ jvm-core/src/main/java/way/architect/jvm/core/clazz/TestClass.java
@@ -0,0 +1,11 @@
+package way.architect.jvm.core.clazz;
+
+public class TestClass {
+
+    private int m;
+
+    private int inc() {
+        return m + 1;
+    }
+
+}
---
+++ jvm-core/src/main/java/way/architect/jvm/core/clazz/TestJar.java
@@ -0,0 +1,8 @@
+package way.architect.jvm.core.clazz;
+
+public class TestJar {
+
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
