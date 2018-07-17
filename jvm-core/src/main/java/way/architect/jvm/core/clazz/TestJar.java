6d26e39f296ed43c490aa5060c5b16a61e7127af
jiangtao <jiangtao@jiangtaodeMacBook-Pro.local>
2018-07-17 14:34:33 +0800

    gradle

--- jvm-core/src/main/java/way/architect/jvm/core/clazz/TestJar.java
+++ jvm-core/src/main/java/way/architect/jvm/core/clazz/TestJar.java
@@ -3,6 +3,6 @@ package way.architect.jvm.core.clazz;
 public class TestJar {
 
     public static void main(String[] args) {
-        System.out.println("args = " + args);
+        System.out.println("args = " + args[0]);
     }
 }
