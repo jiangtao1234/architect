2585c6c01fb5346138e66deb7022076e9d86c5e3
jiangtao <jiangtao@jiangtaodeMacBook-Pro.local>
2018-07-17 15:37:48 +0800

    master

--- jvm-core/src/main/java/way/architect/jvm/core/clazz/TestJar.java
+++ jvm-core/src/main/java/way/architect/jvm/core/clazz/TestJar.java
@@ -1,8 +1,12 @@
 package way.architect.jvm.core.clazz;
 
+
+import org.apache.commons.lang3.StringUtils;
+
 public class TestJar {
 
     public static void main(String[] args) {
         System.out.println("args = " + args[0]);
+        System.out.println("StringUtils.equals(\"HelloWorld\", args[0]) = " + StringUtils.equals("HelloWorld", args[0]));
     }
 }
